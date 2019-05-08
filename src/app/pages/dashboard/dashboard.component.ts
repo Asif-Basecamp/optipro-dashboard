bba573eeaf719c577e9bf00eabfa265689268c45
Asif Ansari <asif.basecamp@gmail.com>
2019-05-08 13:43:21 +0530

    New UI changes on dashboard.component.html

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -87,6 +87,7 @@ export class DashboardComponent implements OnInit{
   public gridStatus: boolean = true;
   public nodes: any = [];
   public transactions: any = [];
+  public searchCriteria: boolean = false;
  
 
   constructor(private dialogService: NbDialogService,private dash:DashboardService ) {
@@ -367,7 +368,7 @@ export class DashboardComponent implements OnInit{
 
   }
 
-   GetExplosion(){
+   GetExplosion(event){
 
     this.dash.GetLotExplosionData('http://localhost:41807','',this.ItemValue,this.DfltWarehouse,this.DistNumFrom,this.DistNumTo,'DOWN').subscribe(
       data =>
@@ -770,6 +771,8 @@ export class DashboardComponent implements OnInit{
         console.log(error);
      }
     )
+
+    this.searchCriteriaToggle(event);
   }
 
   open(dialog: TemplateRef<any>) {
@@ -910,6 +913,56 @@ export class DashboardComponent implements OnInit{
 
   process(){
     this.gridStatus = !this.gridStatus;
+  }
+
+  //Search criteria expand-shrink function  
+  searchCriteriaToggle(event){
+    event.stopPropagation();
+    if(document.getElementById("dashboard-left").classList.contains('shrink')){      
+      document.getElementById("dashboard-left").classList.remove('shrink');
+      document.getElementById("selection-criteria-body").style.height= '100%';
+      document.getElementById("selection-criteria-body").style.display= 'flex';
+      this.searchCriteria = false;
+    }else{
+      document.getElementById("dashboard-left").classList.add('shrink');      
+      document.getElementById("selection-criteria-body").style.height= '0';
+      document.getElementById("selection-criteria-body").style.display= 'none'; 
+      this.searchCriteria = true;
+    }
+  }
+  searchCriteriaExpand(){
+    if(this.searchCriteria && document.getElementById("dashboard-left").classList.contains('shrink')){      
+      document.getElementById("dashboard-left").classList.remove('shrink');
+      document.getElementById("selection-criteria-body").style.height= '100%';
+      document.getElementById("selection-criteria-body").style.display= 'flex';
+    }
+  }
+  
+
+  //Custom accordian function
+  customAccordianGrid(e){
+    if(document.getElementById("grid-accordian").classList.contains('expanded')){
+      this.hideAcordian(e);
+    }else{
+      this.expandAcordian(e);
+    }
+  }
+  customAccordianAnalysis(e){
+    if(document.getElementById("analysis-accordian").classList.contains('expanded')){
+      this.hideAcordian(e);
+    }else{
+      this.expandAcordian(e);
+    } 
+  }
+  hideAcordian(e: any){
+    e.currentTarget.parentElement.parentElement.classList.remove('expanded')
+    e.currentTarget.nextSibling.style.height= '0';
+    e.currentTarget.nextSibling.style.display= 'none';
+  }
+  expandAcordian(e:any){
+    e.currentTarget.parentElement.parentElement.classList.add('expanded')
+    e.currentTarget.nextSibling.style.height= '100%';
+    e.currentTarget.nextSibling.style.display= 'flex';
   }
 
 }
