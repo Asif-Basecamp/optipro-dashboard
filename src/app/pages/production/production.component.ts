57edea1b9c238babca625302cd20cec2d30e637c
Sheetal Parihar <sparihar@optiproerp.com>
2019-06-12 12:31:03 +0530

    Done changes of Inventory Shortage button

--- src/app/pages/production/production.component.ts
+++ src/app/pages/production/production.component.ts
@@ -89,6 +89,9 @@ export interface TreeNode {
    public WOSelected: any;
    public Hour: any;
    Hours: any;
+   public RadioBtnInventShort: any = 'Warehouse';
+   public WHInventShort: boolean = false;
+   public CmpInventShort: boolean = false;
 
   constructor(private dialogService: NbDialogService,private dash: DashboardService,private prod: ProductionService,private toastrService: NbToastrService) {}
   viewOptions = [
@@ -175,17 +178,17 @@ export interface TreeNode {
     })
  }
 
- showDetailsInStockLookup(data,check){
+ showDetailsInStockLookup(Inputdata,check,allGrid){
    let ItemType = '';
-   if(data.ISSERIALTRACKED == 'Y'){
+   if(Inputdata.ISSERIALTRACKED == 'Y'){
      ItemType = 'serial';
    }
-   else if(data.ISBATCHTRACKED == 'Y'){
+   else if(Inputdata.ISBATCHTRACKED == 'Y'){
      ItemType = 'batch';
    }
 
    if(check == 'WH'){
-     this.prod.GetWarehouseWiseInStockQtyDetails(this.arrConfigData.optiProDashboardAPIURL, this.CompanyDB, data.U_O_COMPID, data.U_O_ISSWH,ItemType).subscribe(
+     this.prod.GetWarehouseWiseInStockQtyDetails(this.arrConfigData.optiProDashboardAPIURL, this.CompanyDB, Inputdata.U_O_COMPID, Inputdata.U_O_ISSWH,ItemType).subscribe(
        data => {
           if(data != undefined && data != null){
            if(data.length > 0){
@@ -207,7 +210,14 @@ export interface TreeNode {
       })
    }
    else  if(check == 'CP'){
-     this.prod.GetInStockQtyDetails(this.arrConfigData.optiProDashboardAPIURL, this.CompanyDB, data.U_O_COMPID,ItemType).subscribe(
+
+    let itemCode = '';
+    if(allGrid == true)
+    itemCode = Inputdata.ItemCode;
+     else
+    itemCode = Inputdata.U_O_COMPID;
+
+     this.prod.GetInStockQtyDetails(this.arrConfigData.optiProDashboardAPIURL, this.CompanyDB, itemCode, ItemType).subscribe(
        data => {         
          if(data != undefined && data != null){
            if(data.length > 0){
@@ -285,10 +295,10 @@ export interface TreeNode {
      })
     }
 
- showDetailsOnOrderLookup(data,check){
+ showDetailsOnOrderLookup(Inputdata,check,allGrid){
    if(check == 'WH'){
      
-     this.prod.GetWarehouseWiseOnOrderQtyDetails(this.arrConfigData.optiProDashboardAPIURL, this.CompanyDB, data.U_O_COMPID, data.U_O_ISSWH).subscribe(
+     this.prod.GetWarehouseWiseOnOrderQtyDetails(this.arrConfigData.optiProDashboardAPIURL, this.CompanyDB, Inputdata.U_O_COMPID, Inputdata.U_O_ISSWH).subscribe(
        data => {
         if(data != undefined && data != null){
           if(data.length > 0){
@@ -311,7 +321,13 @@ export interface TreeNode {
 
    }
    else if(check == 'CP'){
-     this.prod.GetOnOrderQtyDetails(this.arrConfigData.optiProDashboardAPIURL, this.CompanyDB, data.U_O_COMPID).subscribe(
+     let itemCode = '';
+     if(allGrid == true)
+     itemCode = Inputdata.ItemCode;
+      else
+     itemCode = Inputdata.U_O_COMPID;
+
+     this.prod.GetOnOrderQtyDetails(this.arrConfigData.optiProDashboardAPIURL, this.CompanyDB, itemCode).subscribe(
        data => {
         if(data != undefined && data != null){
           if(data.length > 0){
@@ -472,6 +488,7 @@ export interface TreeNode {
               else
               this.showgridMaterialPage = false;             
             } 
+
             this.loading = false;         
             
         },
@@ -712,6 +729,16 @@ getMaterialRadioClick(evt){
   }  
  }
 
+ getInventshortRadioClick(){
+  if(this.RadioBtnInventShort == 'Warehouse'){
+    this.WHInventShort = true;
+    this.CmpInventShort = false;
+  }
+  else if(this.RadioBtnInventShort == 'Company'){
+    this.CmpInventShort = true;
+    this.WHInventShort = false;
+  }
+ }
 
 }
 
