9294572e2d43a0ff854c3f43e1547d9c93b45c8e
Monika Chauhan <mchauhan@optiproerp.com>
2019-06-20 19:11:02 +0530

    changes in dashboard

--- src/app/pages/production/production.component.ts
+++ src/app/pages/production/production.component.ts
@@ -101,13 +101,10 @@ export interface TreeNode {
    public hours: any;
    public minutes: any;
    public seconds: any;
-   
+   public viewOptions: any;
+   public viewOption: any;
+
   constructor(private router: Router, private datePipe: DatePipe, private dialogService: NbDialogService,private dash: DashboardService,private prod: ProductionService,private toastrService: NbToastrService) {}
-  viewOptions = [
-    { value: 'SIMPLE', label: 'Simple View' },
-    { value: 'Multi', label: 'Detailed View' },
-  ];
-  viewOption = 'SIMPLE';   
   
   ngOnInit() { 
    this.language = JSON.parse(window.localStorage.getItem('language'));
@@ -127,6 +124,12 @@ export interface TreeNode {
    this.getItemData(this.arrConfigData.optiProDashboardAPIURL, this.CompanyDB);  
    let WoSelect = [];
    this.WOSelected = (e: RowArgs) => WoSelect.indexOf(e.dataItem.DocEntry) >=0 ;
+
+  this.viewOptions = [
+    { value: 'SIMPLE', label: this.language.simple_View },
+    { value: 'Multi', label: this.language.detail_view },
+  ];
+  this.viewOption = 'SIMPLE';  
   }
 
   open(dialog: TemplateRef < any > ) {
